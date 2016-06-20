7e568a98c8b0edc5d223fe5e84dbbecddaba883a
claytondetke <cjdet_000@10.35.48.35>
2016-06-20 15:43:13 -0400

    Added Librarian class

---
+++ Librarian.py
@@ -0,0 +1,8 @@
+class Librarian(Character):
+
+    self.className = 'Librarian'
+    self.goal = 'Seek and organize books while seeking a quiet place to read'
+    self.abilityDefinition = 'Sort the books in this room'
+    self.minigameName = 'Sort books'
+    self.abilSuccess = 'You successfully sort the books'
+    self.abilLoss = 'You fail to sort the books'
