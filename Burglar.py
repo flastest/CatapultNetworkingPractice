60f1318f48083e7fc826bf271488f7d534b02f2e
claytondetke <cjdet_000@10.35.48.35>
2016-06-20 15:31:03 -0400

    added burglar class and updated character abstract

---
+++ Burglar.py
@@ -0,0 +1,8 @@
+class Burglar(Character):
+
+    self.className = 'Burglar'
+    self.goal = 'Steal as much as you can, while evading detection.'
+    self.abilityDefinition = 'Pick the lock that stands in your way.'
+    self.minigameName = 'Lockpick'
+    self.abilSuccess = 'You successfully pick the lock.'
+    self.abilLoss = 'You fail to pick the lock.'
--- Character.py
+++ Character.py
@@ -1,16 +1,16 @@
 class Character:
     """An outline or super-class for each character's individualized class/role""""
 
-    className = 'the role of the character'
-    goal = 'what each character should be trying to do'
-    abilityDefinition = 'what the ability of the character does'
-    minigameName = 'the name of the ability and minigame'
-    abilSuccess = 'what is said when the ability minigame is won'
-    abilLoss = 'what is said when the ability minigame is won'
-    gamePoints = 0
-    playerNumber = -1
+    self.className = 'the role of the character'
+    self.goal = 'what each character should be trying to do'
+    self.abilityDefinition = 'what the ability of the character does'
+    self.minigameName = 'the name of the ability and minigame'
+    self.abilSuccess = 'what is said when the ability minigame is won'
+    self.abilLoss = 'what is said when the ability minigame is won'
+    self.gamePoints = 0
+    self.playerNumber = -1
 
-    def __init__(self, playerNum, ):
+    def __init__(self, playerNum):
         self.playerNumber = playerNum
 
     def __str__(self):
