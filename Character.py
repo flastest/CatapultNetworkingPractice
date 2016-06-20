1eba8635e01fc00f5829f7496b0427fa0d855b64
claytondetke <cjdet_000@10.35.48.35>
2016-06-20 14:24:22 -0400

    Abstract of class
    Don't edit this, copy and change it to create classes

---
+++ Character.py
@@ -0,0 +1,41 @@
+class Character:
+    """An outline or super-class for each character's individualized class/role""""
+
+    className = 'the role of the character'
+    goal = 'what each character should be trying to do'
+    abilityDefinition = 'what the ability of the character does'
+    minigameName = 'the name of the ability and minigame'
+    abilSuccess = 'what is said when the ability minigame is won'
+    abilLoss = 'what is said when the ability minigame is won'
+    gamePoints = 0
+    playerNumber = -1
+
+    def __init__(self, playerNum, ):
+        self.playerNumber = playerNum
+
+    def __str__(self):
+        return self.className
+
+    def getGoal(self):
+        return self.goal
+    
+    def getAbilityDefinition(self):
+        return self.abilityDefinition
+    
+    def getAbilityName(self):
+        return self.minigameName
+    
+    def getMinigameWinText(self):
+        return self.abilSuccess
+    
+    def getMinigameLossText(self):
+        return self.abilLoss
+    
+    def getPlayerNumber(self):
+        return self.playerNumber
+    
+    def getPoints(self, num):
+        self.gamePoints += num
+    
+    def losePoints(self, num):
+        self.gamePoints -= num
