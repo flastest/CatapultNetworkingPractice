de8eac00cc657fb648a74d7a75f37e867dbc72ea
Ariel the Phlastest <pleasantbowel5yndrome@gmail.com>
2016-06-20 15:11:01 -0700

    goddamn branches

---
+++ McAgeeCustomerSupport.py
@@ -0,0 +1,11 @@
+class McAgeeCustomerSupport(Character):
+
+
+    self.className = 'AntiVirus Customer Support'
+    self.goal = 'get rid of the nagging customer'
+    self.abilityDefinition = 'figure out to type "HelloWorld" with a screwed-up keyboard'
+    self.minigameName = '"HelloWorld"'
+    self.abilSuccess = 'You successfully provided minimal assistance to your helpless customer'
+    self.abilLoss = 'You fail to defeat the notorious hacker'
+    self.gamePoints = 0
+    self.playerNumber = -1
