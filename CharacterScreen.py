70a34e5399c1eb59f83acbe4ad261fffc38345e8
MatthewGaby <pixelball1213@gmail.com>
2016-06-22 15:31:06 -0400

    added imports to characterScreen

---
+++ CharacterScreen.py
@@ -0,0 +1,9 @@
+import pygame
+from pygame import *
+class CharacterScreen (self, window, Character, x, y)
+        window = pygame.display.set_mode([x,y])
+        Character.getName() = Name
+        Character.getGoal() = Goal
+        print(Name)
+        print(Goal)
+        
