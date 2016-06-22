3e62bc93e5fc8254981dcb25a1ab0404e94a6826
MatthewGaby <pixelball1213@gmail.com>
2016-06-22 16:00:15 -0400

    changed charcterScreen.py

--- CharacterScreen.py
+++ CharacterScreen.py
@@ -1,6 +1,7 @@
 import pygame
 from pygame import *
-class CharacterScreen (self, window, Character, x, y)
+class CharacterScreen:
+    def __init__ (self, window, Character, x, y):
         window = pygame.display.set_mode([x,y])
         Character.getName() = Name
         Character.getGoal() = Goal
