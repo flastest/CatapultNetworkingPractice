2c7667da2510cf949d90ac32ee9f0a9a021c0ef1
MatthewGaby <pixelball1213@gmail.com>
2016-06-22 15:03:58 -0400

    Title Screen Class

---
+++ TitleScreen.py
@@ -0,0 +1,12 @@
+import pygame
+from pygame.locals import *
+
+class TitleScreen:
+    def __init__(self, window,x,y):
+        image = pygame.image.load('TitleScreen.png')
+        window = pygame.display.set_mode([x,y])
+        x=1500
+        y=1500
+        
+
+        
