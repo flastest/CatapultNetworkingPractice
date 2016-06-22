822f198da4bc2e33fa96a3ce15cba01851f4dfd6
flastest <pleasantbowel5yndrome@gmail.com>
2016-06-22 16:13:00 -0700

    turned images.py into a class

--- images.py
+++ images.py
@@ -1,21 +1,37 @@
-import pygame
+import pygame, time
 from pygame.locals import *
-img = pygame.image.load('Officer.png')
 
-x,y, = 0,0
+class images:
+    img = pygame.image.load('Officer.png')
 
-screen = pygame.display.set_mode([800,800])
-pygame.display.set_caption("My Window")
-running = 1
 
-def putAt(newX,newY):
-    x = newX 
-    y = newY
+    def __init__(self):
+        self.x,self.y = 0,0
 
-    while running:
-        
-        screen.blit(img,(x,y))
+
+    #screen = pygame.display.set_mode([800,800])
+    #pygame.display.set_caption("My Window")
+
+    def display(self,newX,newY,window):
+        x = newX 
+        y = newY
+
+        window.blit(self.img,(x,y))
         pygame.display.flip()
 
 
-putAt(100,100)
+
+        
+
+screen = pygame.display.set_mode([800,800])
+pygame.display.set_caption("My Window")
+police = images()
+police.display(100,100,screen)
+while True:
+    events = pygame.event.get()
+    for event in events:
+        if event.type == QUIT:
+            pygame.quit()
+        
+
+
