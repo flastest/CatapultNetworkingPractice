2a007ed9203825adcbcf74a67e5b3415e13b291d
flastest <pleasantbowel5yndrome@gmail.com>
2016-06-22 15:28:32 -0700

    added and initialized Chef class

---
+++ Chef.py
@@ -0,0 +1,9 @@
+import Character
+
+class Chef(Character.Character):
+    className = 'Chef'
+    goal = 'Hunt for the perfect ingredients to complete your sublime recipe for angel hair pie.'
+    abilityDefinition = 'Find ingredients and preserve them to use in your recipe.'
+    minigameName = 'Find Food'
+    abilSuccess = 'You successfully preserved the ingredients.'
+    abilLoss = 'You fail to obtain new ingredients.'
