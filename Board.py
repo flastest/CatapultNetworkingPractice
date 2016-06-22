6a0e003c10cb782c0bd1dea63367dc9546c2079c
saehejkang <saehej.kang@gmail.com>
2016-06-22 14:44:53 -0400

    more code added

--- Board.py
+++ Board.py
@@ -1,9 +1,27 @@
 import pygame, sys
 from pygame.locals import *
+from math import pi
 pygame.init()
 
 #Settings for board
-
-width = 50
-height = 50
+WHITE = [255,255,255]
+GREEN = [0,255, 0]
+size = (400, 300)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption('WELCOME TO THE GAME')
 fps = 60
+pygame.display.set_mode(size)
+done = True
+while done:
+    screen.fill(WHITE)
+    pygame.draw.line(screen, GREEN, (0, 0), (50,30), 1)
+    events = pygame.event.get()
+    for event in events:
+        if event.type == QUIT:
+            done = False
+            
+
+    
+
+
+
