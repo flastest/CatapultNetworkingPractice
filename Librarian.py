0526cb0267af51479090bfa22a05a4f029679739
Kerororo <arieldayna@gmail.com>
2016-06-23 15:19:38 -0700

    added a lot of book titles and they're all good so don't delete them please

--- Librarian.py
+++ Librarian.py
@@ -59,7 +59,14 @@ class Librarian(Character.Character):
         # list of shitty titles to use
         titleList = ["Github? More like Bukkake","Erotic Sonic Fanfiction", "Comprehensive Guide to Windows 95", "Why Steve Jobs Really Died", "Illuminati Confirmed","Pain in the Ass: Bridge for Beginners","The Middle-Age Testament","Clean Your Hair Out of The Group Shower",
                 "Starbucks and the Making of an Empire","Porch Monkey","Human Trafficking for the Elderly","History of Sarah Palin","Breyer Horse Figurine Catalog","How to Make a Meme-Based Lubricant","Celebrating 75 Years","The Significance of 966"]
-        titleList2 = ["DSM IV-TR","Delete system32","WISC III","A Comprehensive Guide to the Rorschach","","","","","","","","","","","","Pepe"]
+        titleList2 = ["DSM IV-TR","Delete system32","Cosmopolitan","A Comprehensive Guide to the Rorschach","How to Make a Solar Hooker from Scratch","Julius Caesar Salad","Pokemon Emerald","Lord of the Fries","To Kill a Meme-ing Bird","Of Mice and Memes","Catch-966","Anon in an Anonymous Land",            "Bae-o-wulf","Alice's Adventures in Reddit","Prunes and Prejudice","The Mediocre Gatsby"]
+        titleList3 = ["Another Young Adult Vampire Romance Novel","War and Pepe","The Fault in our Apple Devices","The Adventures of Blackberry Phone","Lolita","The Lion King","Nineteen-Eighty-Three-Point-Nine-Six-Six","Gone with the Meme","Pomegranates of Wrath","The CATAPULTry Tales",
+                "Billy Joel's Greatest Hits'","The Merchant of Chinatown","Much Ado about Cookies","Less Miserables","Old Man and The Book that Dragged on Forever","Hoarder of the Things"]
+        titleList4 = ["Charlotte the Pleb","One Flew Over The Cuckoo's Nest","Twenty Thousand Leagues into the Internet","The Strange Case of Dr. Jekyll and Mr. Mime","A Clockwork Pepe","Call of the Meme","Through the Computer Monitor","All Quiet on the Viral Front",
+                "The Lion, The Witch and the Unpacked Suitcase","Love in the Time of Zika","The Prelude/Angry Young Man","Journey to the Center of Walmart","A Midsummer's Night Meme","Uncle Tom's Beach Resort","If I Forget Thee, Jerusalem","Just Start Mushing it Slowly but Erotically"]
+        # Other names:
+        #   A Wrinkle in Spine ...im tired lol
+
 
         for b in range(numBooks):
             red = random.randint(50,255)
@@ -69,7 +76,7 @@ class Librarian(Character.Character):
             print(b)
             i = Book(bookloc)
             i.setColor(red,blue,green)
-            i.addText(titleList2[b])
+            i.addText(titleList4[b])
             bookList.append(i)
             i.draw()
             bookloc += 75
@@ -86,7 +93,7 @@ class Librarian(Character.Character):
         
 
 screen = pygame.display.set_mode([1200,800])
-pygame.display.set_caption("My Window")
+pygame.display.set_caption("( ͡° ͜ʖ ͡°)")
 kek = Librarian(966)
 kek.minigame(screen)
 
