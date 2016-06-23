912c54809bfbeb400b8c8574e8a3eb5e764974d3
claytondetke <cjdet_000@10.35.48.35>
2016-06-23 13:05:41 -0400

    Update to minigame in McAg

--- McAgeeCustomerSupport.py
+++ McAgeeCustomerSupport.py
@@ -14,23 +14,43 @@ class McAgeeCustomerSupport(Character.Character):
     self.playerNumber = -1
 
     def minigame(self):
+        letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s', 't','u','v','w','x','y','z']
+        random.shuffle(letters)
+        random.shuffle(letters)
+
         response = ''
+        letterPosition = 0
+        letterSize = 20
         isUprCase = False
 
         minigameWon = False
         minigameLost = False
+        startTime = time.clock()
+
+        window = pygame.display.set_mode([400,200])
+        pygame.display.set_caption('"cmd"')
+
         while not minigameWon and not minigameLost:
+            x = time.clock()
             events = pygame.event.get()
             for event in events:
                 if event.type == QUIT:
-                    return
+                    window.close()
+                if event.type == KEYUP:
+                    if event.key == K_LSHIFT or event.key == K_RSHIFT:
+                        isUprCase = False
                 if event.type == KEYDOWN:
+                    if event.key == SPACE:
+                        self.insert(letterPosition, response, ' ')
+                        letterPosition += 1
+                    if event.key == K_LSHIFT or event.key == K_RSHIFT:
+                        isUprCase = True
                     if event.key == K_LEFT:
-                        xPos += spaceBetweenLetters
+                        if letterPosition > 0:
+                            letterPosition -= 1
                     if event.key == K_RIGHT:
-                        xPos -= spaceBetweenLetters
-                    if event.key == K_1:
-                        isCircle = False
-                    if event.key == K_2:
-                        isCircle = True
-                window.fill((0,0,0))
+                        if letterPosition < len(response):
+                            letterPosition += 1 
+                window.fill((255,255,255))
+                pygame.draw.line(window, (0,0,0), (10+letterSize*letterPosition, 30), (10+letterSize*(letterPosition+1), 30), 2)
+                pygame.display.update()
