b2a82e3e432ce384e1ce076c6a536f83ece0cc4b
flastest <pleasantbowel5yndrome@gmail.com>
2016-06-25 09:47:13 -0700

    started work on fixing TitleScreen.py, may need backup reinforcements

--- TitleScreen.py
+++ TitleScreen.py
@@ -2,9 +2,10 @@ import pygame
 from pygame.locals import *
 
 class TitleScreen:
-    def __init__(self, window,x,y):
-        image = pygame.image.load('TitleScreen.png')
-        window = pygame.display.set_mode([x,y])
-        x=1500
-        y=1500
+    def __init__(self):
+        self.image = pygame.image.load('TitleScreen.png')
+        
+    def display(self,window):
+        window = pygame.display.set_mode([0,0])
+        window.blit(self.image,(0,0))
     
