5c100380c67b50cf53ce1ed72dea88f0f50b0a64
claytondetke <cjdet_000@10.35.48.35>
2016-06-24 21:52:34 -0400

    Completed Hacker character

--- Hacker.py
+++ Hacker.py
@@ -31,11 +31,25 @@ class Hacker(Character.Character):
         red = [255, 100, 0]
         green = [0,255,0]
         blue = [0,0,255]
-        wall = []
+        wall = [600,200]
+        fwW = 300
+        fwH = 150
+        dFw = []
+        fwAtt = []
         player = [600, 615]
         attackTime = 0
+        fwDMove = 0
+        fwMoveTime = 0
+        fwTime = time.clock() + 1
+        fwAttTime = time.clock() + 1
+        fwGval = 100
+        fwGval = 0
         attacks = []
         attTi = time.clock() + .02
+        lines = []
+        nextTime = False
+        for i in range(16):
+            lines.append([0,0,0,0,255,0,0])
         hp = 20
         tP = 0
         ended = False
@@ -65,35 +79,117 @@ class Hacker(Character.Character):
                         spHeld = True
                         if attackTime < time.clock():
                             attacks.append((player[0], player[1]-25))
-                            attackTime = time.clock() + .5
+                            attackTime = time.clock() + .7
                     tP = time.clock()
             if spHeld:
                 if attackTime < time.clock():
-                    attackTime = time.clock()+.5
+                    attackTime = time.clock()+.7
                     attacks.append((player[0],player[1]-20))
             if tP < time.clock():
-                tP = time.clock()+.02
+                tP = time.clock()+.05
                 if rIsPressed:
                     player[0] += 5
                 if lIsPressed:
                     player[0] -= 5
+            if fwDMove < time.clock():
+                fwDMove = time.clock() + 10
+                xVal = 8
+                yVal = 6
+                minVal = 3
+                if time.clock() > startTime + 35:
+                    xVal = 12
+                    yVal = 10
+                    minVal = 6
+                dx = random.randint(-xVal,xVal)
+                dy = random.randint(-yVal,yVal)
+                while abs(dx) < minVal:
+                    dx = random.randint(-xVal,xVal)
+                while abs(dy) < minVal:
+                    dy = random.randint(-yVal,yVal)
+                dFw = [dx, dy]
+            if fwMoveTime < time.clock():
+                fwMoveTime = time.clock() + .03
+                wall = [wall[0] + dFw[0], wall[1] + dFw[1]]
+                if wall[0] <= fwW/2:
+                    wall[0] = fwW/2
+                    dFw[0] = -dFw[0]
+                if wall[0] >= 1200-fwW/2:
+                    wall[0] = 1200-fwW/2
+                    dFw[0] = -dFw[0]
+                if wall[1] >= 400-fwH/2:
+                    wall[1] = 400-fwH/2
+                    dFw[1] = -dFw[1]
+                if wall[1] <= 60+fwH/2:
+                    wall[1] = 60+fwH/2
+                    dFw[1] = -dFw[1]
+                if nextTime:
+                    lines[i][0] = lines[i][0] + dFw[0]
+                    lines[i][1] = lines[i][1] + dFw[1]
+                    lines[i][2] = lines[i][2] + dFw[0]
+                    lines[i][3] = lines[i][3] + dFw[1]
+                    nextTime = not nextTime
+                else:
+                    for i in range(len(lines)):
+                        fwGval = random.randint(20,120)
+                        fwBval = random.randint(0,20)
+                        lines[i][0] = random.randint(wall[0]-10-fwW/2, wall[0]+10+fwW/2)
+                        lines[i][1] = random.randint(wall[1]-10-fwH/2, wall[1]+10+fwH/2)
+                        lines[i][2] = random.randint(wall[0]-10-fwW/2, wall[0]+10+fwW/2)
+                        lines[i][3] = random.randint(wall[1]-10-fwH/2, wall[1]+10+fwH/2)
+                        lines[i][5] = random.randint(0,100)
+                        lines[i][6] = random.randint(0,30)
+                    nextTime = not nextTime
+            if fwTime < time.clock():
+                x = .6
+                if time.clock() > startTime + 40:
+                    x = .3
+                fwTime = time.clock()+x
+                spawnY = wall[1]+(fwH/2) + 5
+                attDX = (player[0]-wall[0])//72
+                attDY = (player[1]-spawnY)//72
+                fwAtt.append([wall[0], spawnY, int(attDX), int(attDY)])
             if player[0] <= 5:
                 player[0] = 5
             if player[0] >= 1195:
                 player[0] = 1195
             window.fill(black)
+            if fwAttTime < time.clock():
+                x = .05
+                if time.clock() > 40:
+                    x = .03
+                fwAttTime = time.clock() + x
+                for i in range(len(fwAtt)):
+                    if fwAtt[i][1] > 800:
+                        del fwAtt[i]
+                        break
+                for i in range(len(fwAtt)):
+                    fwAtt[i][0] = fwAtt[i][0]+fwAtt[i][2]
+                    fwAtt[i][1] = fwAtt[i][1]+fwAtt[i][3]
+                    if fwAtt[i][0] >= player[0]-8 and fwAtt[i][0] <= player[0]+8 and fwAtt[i][1] >= player[1]-10 and fwAtt[i][1] <= player[1]+10:
+                        minigameLost = True
             if time.clock() > attTi:
-                attTi = time.clock() + .02
+                attTi = time.clock() + .03
                 for i in range(len(attacks)):
                     if attacks[i][1] < 60:
                         del attacks[i]
                         break
+                for i in range(len(attacks)):
                     attacks[i] = (attacks[i][0], attacks[i][1]-5)
+                    if attacks[i][1] <= wall[1]+fwH/2 and attacks[i][1] >= wall[1]-fwH and attacks[i][0] >= wall[0]-fwW/2 and attacks[i][0] <= wall[0]+fwW/2:
+                        hp -= 1
+                        del attacks[i]
+                        break
             for i in range(len(attacks)):
-                pygame.draw.circle(window, blue, attacks[i], 3)
+                pygame.draw.circle(window, blue, attacks[i], 5)
+            for i in range(len(fwAtt)):
+                pygame.draw.circle(window, rd, (int(fwAtt[i][0]), int(fwAtt[i][1])), 3)
+                pygame.draw.line(window, rd, (fwAtt[i][0],fwAtt[i][1]), (fwAtt[i][0]-2*fwAtt[i][2], fwAtt[i][1]-3*fwAtt[i][3]), 3)
             pygame.draw.rect(window, rd, (0,0,1200,60))
             pygame.draw.rect(window, green, (0,0, hp*60,60))
             pygame.draw.polygon(window,white,((player[0],player[1]-15),(player[0]-10, player[1]+10), (player[0]+10,player[1]+10)),0)
+            pygame.draw.rect(window, (255, fwGval, fwBval), (wall[0]-fwW/2, wall[1]-fwH/2, fwW, fwH))
+            for i in range(len(lines)):
+                pygame.draw.line(window, (lines[i][4],lines[i][5],lines[i][6]), (lines[i][0],lines[i][1]), (lines[i][2],lines[i][3]), random.randint(1,5))
             font = pygame.font.Font(None, 28)
             text = font.render(str(int(self.timeLimit+startTime-time.clock())), 1, black)
             txt = font.render('Use the arrowkeys to move and the spacebar to fire.', 1, black)
